c843c9fc9771b86ddf46570b13d247c2447a4483
Danonche <larion.dli92@gmail.com>
2023-07-30 13:29:30 +0300

    add correct slkrt

--- test_items.py
+++ test_items.py
@@ -7,6 +7,6 @@ def test_add_basket(browser):
     browser.get(link)
     time.sleep(5)
     button = None
-    button = browser.find_elements(By.CSS_SELECTOR, "button.btn.btn-lg.btn-primarys")
+    button = browser.find_elements(By.CSS_SELECTOR, "button.btn.btn-lg.btn-primary")
     assert button, "!!---no item (:---!!"
  
