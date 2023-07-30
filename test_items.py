0530550015b7954b8916f6e973bd9fddcdbfe73c
Danonche <larion.dli92@gmail.com>
2023-07-30 12:40:55 +0300

    add correct assert method

--- test_items.py
+++ test_items.py
@@ -1,18 +1,12 @@
-from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.by import By
-# import pytest
 import time
-
 
 link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
 
-def test_edd_basket(browser):
-  try:
+def test_add_basket(browser):
     browser.get(link)
-    time.sleep(30)
-  
+    time.sleep(5)
     button = None
-    button = browser.find_element(By.CSS_SELECTOR, "button.btn.btn-lg.btn-primarys")
-    assert button != None, "Кнопка на месте"
-  except NoSuchElementException:
-      pass
+    button = browser.find_elements(By.CSS_SELECTOR, "button.btn.btn-lg.btn-primarys")
+    assert button, "!!---no item (:---!!"
+ 
